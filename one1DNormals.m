e02a0f29d2939314b8362346a6606b6d88a2114c
MCBurbage <mcfieler@uh.edu>
2017-02-06 11:18:49 -0600

    Completed 1D Normal transition matrix calcuation

--- one1DNormals.m
+++ one1DNormals.m
@@ -1,20 +1,21 @@
+%NOTE:  This only works for an odd number of cells.
+
+
 %initialize variables
 L = 9; %workspace edge length
-cellSize = 1; % 
+cellSize = 1; %
 mu = [L/2 L/2]; % average
-sigma = [L/10 L/10]; 
+sigma = [L/10 L/10]; %standard deviation of distribution
 
-nCells = L/cellSize;
-p = zeros(2,nCells);
+nCells = L/cellSize; %total number of cells
+w = zeros(1,nCells); %initialize the distribution
 
 %calculate percentage of population in each cell
-p(1,1) = normcdf(cellSize,mu(1),sigma(1));
+w(1) = normcdf(cellSize,mu(1),sigma(1));
 for i = 2:nCells-1
-    p(1,i) = (normcdf(i*cellSize,mu(1),sigma(1)) - normcdf((i-1)*cellSize,mu(1),sigma(1)));
+    w(i) = (normcdf(i*cellSize,mu(1),sigma(1)) - normcdf((i-1)*cellSize,mu(1),sigma(1)));
 end
-p(1,nCells) = 1-sum(p(1,:));
-
-wx = p(1,:);
+w(nCells) = 1-sum(sum(w));
 
 %initialize the transition matrices with zeros
 Px = zeros(nCells,nCells);
@@ -23,50 +24,52 @@ Px = zeros(nCells,nCells);
 %unknowns for each row except the first and last which have only two
 %unknowns each
 sz = ceil(nCells/2)*3-2;
-% for loop construct constraints
-% 
+
+%set number of cells in half the distribution
 halfCells = ceil(nCells/2);
 
+%construct constraint equations
 %Fill top of Aeq matrix with probability constraints-each row must sum to 1
 %Initialize with a zero matrix
 AeqTop = zeros(halfCells, sz);
-%The first cell has only two possibilities
+%The first cell has only two possible moves (stay or right)
 AeqTop(1,1:2) = [1,1];
-%The interior cells have three possibilities
+%The interior cells have three possible moves (left, stay, or right)
 cnt = 3;
 for i = 2:halfCells-1
     AeqTop(i,cnt:cnt+2) = [1,1,1];
     cnt=cnt+3;
 end
-%The last cell has only two possibilities but symmetry makes it twice as
-%likely to leave as stay
-AeqTop(halfCells,cnt:cnt+1) =  [2,1]; 
-%Fill top of Beq matrix with probability constraints-each row must sum to 1
+%The last cell has only two possible moves (left or stay) but symmetry 
+%makes it twice as likely to leave as stay
+AeqTop(halfCells,cnt:cnt+1) = [2,1];
+%Fill top of Beq matrix with probability constraints
+%Each row must sum to 1
 BeqTop = ones(halfCells,1);
 
 %Fill bottom of Aeq matrix with wP = w constraints
-AeqBot = zeros(halfCells, sz);  
-%The first row only has two possibilities
-AeqBot(1,1:3) =  [wx(1),0,wx(2)]; 
-%Interior rows have three possibilities
+AeqBot = zeros(halfCells, sz);
+%The first row only has two entries
+AeqBot(1,1:3) = [w(1),0,w(2)];
+%Interior rows have three entries
 cnt = 2;
 for i = 2:halfCells-1
-    AeqBot(i,cnt:cnt+4) = [wx(i-1),0,wx(i),0,wx(i+1)];
+    AeqBot(i,cnt:cnt+4) = [w(i-1),0,w(i),0,w(i+1)];
     cnt = cnt+3;
 end
-%The last row only has two possibilities but one is doubled due to symmetry
-AeqBot(halfCells,cnt:sz) = [2*wx(halfCells-1),0,wx(halfCells)];
+%The last row only has two entries but one is doubled due to symmetry
+AeqBot(halfCells,cnt:sz) = [2*w(halfCells-1),0,w(halfCells)];
 
 %Fill bottom of Aeq matrix with wP = w constraints
-BeqBot = wx(1:halfCells)';
+BeqBot = w(1:halfCells)';
 
 %Concatenate the Aeq and Beq matrices
 Aeq = [AeqTop;AeqBot];
-Beq = [AeqTop;AeqBot];
+Beq = [BeqTop;BeqBot];
 
-%Set the minimization target for the last entry (center of distribution)
+%Set the minimization target for the first entry (edge of distribution)
 f = zeros(sz,1);
-f(end) = 1;  %minimize the last entry
+f(1) = 1;  %minimize the first entry
 
 %Set the bounds on the solutions to be proper probabilities
 %between 0 and 1
@@ -74,19 +77,37 @@ lb = zeros(sz,1);
 ub = ones(sz,1);
 
 %Solve for the values that minimize the last variable
-%x = zeros(sz,1);
 x = linprog(f,[],[],Aeq,Beq,lb,ub);
 
+%Build the transition matrix from the solutions to the linear programming
+%problem
+%The first row has two entries
 Px(1,1:2) = x(1:2);
 cnt = 3;
+%The interior rows have three entries
 for i=2:halfCells-1
     Px(i,i-1:i+1) = x(cnt:cnt+2);
     cnt = cnt+3;
 end
+%The center row has the last two entries
+%The symmetry around the center reverses the order for the rest of the
+%distribution
 Px(halfCells,halfCells-1:halfCells+1) = [x(sz-1) x(sz) x(sz-1)];
 cnt = sz-2;
-for i=halfCells+1:nCells
-    Px(i,i-1:i+1) = x(cnt+2:cnt);
+%The interior rows have three entries
+for i=halfCells+1:nCells-1
+    Px(i,i-1:i+1) = flipud(x(cnt-2:cnt));
     cnt = cnt-3;
 end
-Px(nCells,nCells-1:nCells) = x(2:1);
+%The last row has the last two entries
+Px(nCells,nCells-1:nCells) = [x(2),x(1)];
+
+%Convert the transition matrix to a sparse matrix
+Ps = sparse(Px);
+%Find the eigenvalues
+[V,~] = eigs(Ps');
+%The first column holds the stationary distribution
+st = V(:,1)';
+%Normalize the stationary distribution to be proper probabilities [0,1]
+st = st./sum(st);
+
